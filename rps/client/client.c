f2966ea4b07c9aabc207181be4e40bb190341508
WishMakers0 <kirbydan830@gmail.com>
2021-12-02 12:49:45 -0500

    Fundamental client functions, filling out main()

--- rps/client/client.c
+++ rps/client/client.c
@@ -9,7 +9,7 @@
 	Filename: client.c
 	Purpose: 
 
-	Run command: ./client.out <arguments>
+	Run command: ./client.out <ip_address> <port num>
 	Compile command: 
 		make build_client
         *OR*
@@ -18,7 +18,6 @@
 	<end>
 */
 
-/*
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -26,18 +25,118 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <netwLib.h>
+
+//preprocessor defines
+#define MAXCHARS 256
+
+//enum for constants declared in place of a million preprocessor definitions
+typedef enum MessageType { Ready=1, Go=2, Nick=3, Retry=4, RPS=5, Score=8, Stop=9 };
+
+/*
+Function name: getTrueMessageLength
+Description: 
+
+Parameters: char* buffer - the received full message
+
+Return value: int length - the length of the "true" message within an encoded packet
 */
+int getTrueMessageLength(char* buffer) {
+	//first two characters are always , followed by a number for the type - start indexing at 2
+	int length = 0;
+	bool atFound = false;
+	for(int i = 2; (i < MAXCHARS) || !atFound; i++) {
+		if (buffer[i] == '@') {
+			atFound = true;
+		}
+		else {
+			length++;
+		}
+	}
+	return length;
+}
 
+/*
+Function name: parseMessage
+Description: 
 
+Parameters: char* buffer - the received message to translate
+			MessageType type - the message type to check for
+
+Return value: bool success - success/failure upon parsing message
+*/
+bool parseMessage(char* buffer, MessageType type) {
+	bool result = true;
+	char copyBuffer[MAXCHARS];
+
+	//Sanity check to ensure encoded message's validity.
+	if (buffer[0] != ',') {
+		result = false;
+		//error?
+	}
+
+	//Verifying if the type of the message is what we expect.
+	int encodedType = buffer[1] - '0';
+	if (encodedType != (int)type) {
+		result = false;
+		//error?
+	}
+
+	//Copy "true message" to copyBuffer to strcmp with the expected message.
+	int messageLength = getTrueMessageLength(buffer);
+	strncpy(copyBuffer, &buffer[2], messageLength);
+
+	//Switch statement for different message types.
+	switch(type) {
+		case Ready:
+			if (strncmp(copyBuffer,"READY",5)) {
+				result = true;
+			}
+			break;
+		case Go:
+			if (strncmp(copyBuffer,"GO",2)) {
+				result = true;
+			}
+			break;
+		case Retry:
+			if (strncmp(copyBuffer,"RETRY",5)) {
+				result = true;
+			}
+			break;
+		case RPS:
+			if (strncmp(copyBuffer,"ROCK",4) || strncmp(copyBuffer,"PAPER",5) || strncmp(copyBuffer,"SCISSORS",8)) {
+				//return result to a specific variable if applicable?
+				result = true;
+			}
+			break;
+		case Score:
+			if (strncmp(copyBuffer,"SCORE",5)) {
+				result = true;
+				//process other parts of score in remaining segment of message?
+			}
+			break;
+		case Stop:
+			if (strncmp(copyBuffer,"STOP",4)) {
+				result = true;
+			}
+			break;
+		default:
+			result = false;
+			//error("Non-parsible message type?",,);
+			break;
+	}
+	return result;
+}
 
 /*
 Function name: establishConnection
 Description: 
 
-Parameters: 
+Parameters: char* ip - ip address specified
+			int port - port number specified
 Return value: 
 */
-void establishConnection() {
+void establishConnection(char* ip, int port) {
 
 }
 
@@ -50,7 +149,7 @@ Return value:
 */
 
 void nameEntry() {
-
+	//Obvious note to future self: when parsing nickname options PLEASE make sure there aren't any digits, commas, or at signs
 }
 
 /*
@@ -61,8 +160,12 @@ Parameters:
 Return value: 
 */
 
-void awaitGo() {
-
+void awaitGo(char* buffer) {
+	//recvDelim(socket, buffer, MAXCHARS, 0);
+	if(!parseMessage(buffer, Go)) {
+		//spit out error message and prompt for another message?
+		//Classic Two Generals' Problem - https://www.youtube.com/watch?v=IP-rGJKSZ3s
+	}
 }
 
 /*
@@ -109,8 +212,11 @@ Parameters:
 Return value: 
 */
 
-void gameOverRoutine() {
-
+void gameOverRoutine(char* buffer) {
+	//recvDelim(socket, buffer, MAXCHARS, 0);
+	if(!parseMessage(buffer, Stop)) {
+		//spit out error message and prompt for another message?
+	}
 }
 
 /*
@@ -125,7 +231,7 @@ Purpose of the client:
  - Send a READY message and wait for a response (send number of turns?)
  - Prompt user for a nickname
  - Send nickname in a message and wait for a response (if READY, proceed.  if RETRY, loop back)
- - Send GO message to server, do not wait for a response back
+ - Wait for GO message first.
  - Prompt user for roshambo choice (rock, paper, scissors)
  - Send choice to server and await response of GO or a SCORE
  - If GO, loop back.  If SCORE with info, display to player
@@ -137,6 +243,10 @@ Purpose of the client:
 int main(int argc, char* argv[]) {
 
     //Initialize proper variables
+	char buffer[MAXCHARS]; //generic use text buffer
+	char nickname[MAXCHARS]; //nickname text buffer
+	bool gameFinished = false;
+	int socket; //socket???
 
     // Check number of arguments to ensure correctness
     if (argc != 3) {
@@ -144,6 +254,33 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    //Sequence
+    //Sequence of events
 
+	//Establish connection with server, then send ready message.
+	//establishConnection();
+
+	//Wait for a response back from the server, then verify it.
+	//recvDelim(socket, buffer, MAXCHARS, 0);
+	if(!parseMessage(buffer, Ready)) {
+		//spit out error message and prompt for another message?
+		//Classic Two Generals' Problem - https://www.youtube.com/watch?v=IP-rGJKSZ3s
+	}
+
+	//Prompt user for nickname, send to server, wait for response
+	//nameEntry();
+
+	//Wait for Go message to be received and verified, then proceed with Roshambo
+	//Game loop begins here
+	awaitGo(buffer);
+	while(!gameFinished) {
+		//rpsGameplay();
+		//gameFinished = awaitLoop(&gameFinished);
+	}
+
+	//Wait for a stop message to ensure connection termination, then terminate
+	gameOverRoutine(buffer);
+	
+	//terminate connection formally??  how??
+
+	return 0;
 }
