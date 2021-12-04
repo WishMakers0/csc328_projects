e706747ea9ab92b40b982f8fdf18aff21450e784
WorcestershireSample <93160735+WorcestershireSample@users.noreply.github.com>
2021-12-03 21:42:55 -0500

    Update Library.h

--- rps/libraries/Library.h
+++ rps/libraries/Library.h
@@ -10,7 +10,7 @@ extern void error(char *errMsg, int errType, int errNum);
 // Receives a message, and checks if it received the delimiter.
 // Function has 3 returns: 0 for all is good, -1 for bad recv values (no comma, number, or delimiter), and -2 for failed recv.
 // Parameters are all the parameters for recv, sans the length. Length is statically set to 32.
-extern int recvDelim(int socket*, char *buffer, int flags);
+extern int recvDelim(int socket, char *buffer, int flags);
 
 // Sends a message with requisite additions:
 // Beginning of message, message type, and delimiter value.
@@ -20,9 +20,9 @@ extern int recvDelim(int socket*, char *buffer, int flags);
     // READY is text sent
     // @ is delimiter
     // int socket, void *buffer, size_t length, int flags, int messageType
-extern int sendDelim(int sockfd*, char *buffer, size_t length, int flags, int messageType);
+extern int sendDelim(int sockfd, char *buffer, size_t length, int flags, int messageType);
 
 // recvFinal simply takes the return value of recvDelim and does certain things with it depending on what that value is.
-extern void recvFinal(int socket*, char *buffer, int flags);
+extern void recvFinal(int socket, char *buffer, int flags);
 
 #endif
